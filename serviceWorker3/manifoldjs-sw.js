adbcb9a744ca5f3e3ec738dcc6b4c6973e107184
Marcelo Rodriguez <marcelo.rodriguez@southworks.com>
2017-01-06 10:44:12 -0300

    Added code for service worker #3

--- serviceWorker3/manifoldjs-sw.js
+++ serviceWorker3/manifoldjs-sw.js
@@ -1,13 +1,55 @@
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
+//Install stage sets up the offline page in the cahche and opens a new cache
 self.addEventListener('install', function(event) {
-  //TODO: Add code here
+  event.waitUntil(preLoad());
 });
+
+var preLoad = function(){
+  console.log('[Manifoldjs] Install Event processing');
+  return caches.open('manifoldjs-offline').then(function(cache) {
+    console.log('[Manifoldjs] Cached index and offline page during Install');
+    return cache.addAll(['/offline.html', '/index.html']);
+  });
+}
 
 self.addEventListener('fetch', function(event) {
-  //TODO: Add code here
+  console.log('The service worker is serving the asset.');
+  event.respondWith(checkResponse(event.request).catch(function() {
+    return returnFromCache(event.request)}
+  ));
+  event.waitUntil(addToCache(event.request));
 });
 
-self.addEventListener('refreshOffline', function(response) {
-  //TODO: Add code here
-});
+var checkResponse = function(request){
+  return new Promise(function(fulfill, reject) {
+    fetch(request).then(function(response){
+      if(response.status !== 404) {
+        fulfill(response)
+      } else {
+        reject()
+      }
+    }, reject)
+  });
+};
+
+var addToCache = function(request){
+  return caches.open('manifoldjs-offline').then(function (cache) {
+    return fetch(request).then(function (response) {
+      console.log('[manifoldjs] add page to offline'+response.url)
+      return cache.put(request, response);
+    });
+  });
+};
+
+var returnFromCache = function(request){
+  return caches.open('manifoldjs-offline').then(function (cache) {
+    return cache.match(request).then(function (matching) {
+     if(!matching || matching.status == 404) {
+       return cache.match('offline.html')
+     } else {
+       return matching
+     }
+    });
+  });
+};
