539d1cf30ebcd8832c8f535be0af6fe428bb8688
Anton Podviaznikov <podviaznikov@gmail.com>
2017-02-22 22:22:57 -0800

    fix typo

--- serviceWorker2/manifoldjs-sw.js
+++ serviceWorker2/manifoldjs-sw.js
@@ -1,6 +1,6 @@
 //This is the "Offline copy of pages" wervice worker
 
-//Install stage sets up the index page (home page) in the cahche and opens a new cache
+//Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   var indexPage = new Request('index.html');
   event.waitUntil(
