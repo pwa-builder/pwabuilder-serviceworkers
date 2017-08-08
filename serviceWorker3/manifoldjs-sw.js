447e20c715eebf8c88113dc9a89681f95e7c0ef9
Daniel Thompson <daetho@users.noreply.github.com>
2017-08-08 12:57:58 +0100

    Fix typo

--- serviceWorker3/manifoldjs-sw.js
+++ serviceWorker3/manifoldjs-sw.js
@@ -1,6 +1,6 @@
 //This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-//Install stage sets up the offline page in the cahche and opens a new cache
+//Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener('install', function(event) {
   event.waitUntil(preLoad());
 });
