8905e8b9b24583da5df39a5e8dde6566046e2f56
Erika Doyle Navara <erika.doyle@microsoft.com>
2018-04-16 10:14:33 -0700

    Nit in comment

--- serviceWorker1/pwabuilder-sw.js
+++ serviceWorker1/pwabuilder-sw.js
@@ -24,7 +24,7 @@ self.addEventListener('fetch', function(event) {
     }));
 });
 
-//This is a event that can be fired from your page to tell the SW to update the offline page
+//This is an event that can be fired from your page to tell the SW to update the offline page
 self.addEventListener('refreshOffline', function(response) {
   return caches.open('pwabuilder-offline').then(function(cache) {
     console.log('[PWA Builder] Offline page updated from refreshOffline event: '+ response.url);
