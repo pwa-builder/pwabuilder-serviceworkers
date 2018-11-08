15910e3c8260c8e5a9f16edf6c9d96200b50456a
Lucas Rojas <lucas.rojas@southworks.com>
2018-11-08 14:45:52 -0300

    Add serviceworker 5 advanced pre cache

---
+++ serviceWorker5/pwabuilder-sw-register.js
@@ -0,0 +1,14 @@
+//This is the service worker with the Cache-first network
+
+//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+if (navigator.serviceWorker.controller) {
+  console.log('[PWABuilder] active service worker found, no need to register')
+} else {
+
+//Register the ServiceWorker
+  navigator.serviceWorker.register('pwabuilder-sw.js', {
+    scope: './'
+  }).then(function(reg) {
+    console.log('Service worker has been registered for scope:'+ reg.scope);
+  });
+}
