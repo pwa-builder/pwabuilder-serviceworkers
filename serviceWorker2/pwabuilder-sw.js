428bb180fcdf278c9895d3e8a6b00ad44340f2a3
Jeff Burtoft <boyofgreen@gmail.com>
2018-11-09 14:56:06 -0600

    Update pwabuilder-sw.js

--- serviceWorker2/pwabuilder-sw.js
+++ serviceWorker2/pwabuilder-sw.js
@@ -40,7 +40,7 @@ self.addEventListener('fetch', function(event) {
       return caches.open(cacheName).then(function(cache) {
         return cache.match(event.request).then(function(matching) {
           var report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report;
+          return report
         });
       });
     })
