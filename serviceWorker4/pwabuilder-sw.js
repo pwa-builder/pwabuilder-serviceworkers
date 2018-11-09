48a63ea8606986d438e9130edb04104d02b8327a
Jeff Burtoft <boyofgreen@gmail.com>
2018-11-09 14:57:14 -0600

    Update pwabuilder-sw.js

--- serviceWorker4/pwabuilder-sw.js
+++ serviceWorker4/pwabuilder-sw.js
@@ -55,5 +55,5 @@ function update(request) {
 
 function fromServer(request){
   //this is the fallback if it is not in the cache to go to the server and get it
-  return fetch(request).then(function(response){ return response;});
+  return fetch(request).then(function(response){ return response});
 }
