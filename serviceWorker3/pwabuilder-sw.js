576fc4696b99e8ddc9619c32f058e207dbcba862
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-02-04 18:22:42 -0300

    Update format in SW 3

--- serviceWorker3/pwabuilder-sw.js
+++ serviceWorker3/pwabuilder-sw.js
@@ -1,55 +1,57 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-//Install stage sets up the offline page in the cache and opens a new cache
-self.addEventListener('install', function(event) {
+// Install stage sets up the offline page in the cache and opens a new cache
+self.addEventListener("install", function (event) {
   event.waitUntil(preLoad());
 });
 
-const preLoad = function(){
-  console.log('[PWA Builder] Install Event processing');
-  return caches.open('pwabuilder-offline').then(function(cache) {
-    console.log('[PWA Builder] Cached index and offline page during install');
-    return cache.addAll(['/offline.html', '/index.html']);
+function preLoad() {
+  console.log("[PWA Builder] Install Event processing");
+  return caches.open("pwabuilder-offline").then(function (cache) {
+    console.log("[PWA Builder] Cached index and offline page during install");
+    return cache.addAll(["/offline.html", "/index.html"]);
   });
-};
+}
 
-self.addEventListener('fetch', function(event) {
-  console.log('[PWA Builder] The service worker is serving the asset.');
-  event.respondWith(checkResponse(event.request).catch(function() {
-    return returnFromCache(event.request);}
-  ));
+self.addEventListener("fetch", function (event) {
+  console.log("[PWA Builder] The service worker is serving the asset.");
+  event.respondWith(
+    checkResponse(event.request).catch(function () {
+      return returnFromCache(event.request);
+    })
+  );
   event.waitUntil(addToCache(event.request));
 });
 
-const checkResponse = function(request){
-  return new Promise(function(fulfill, reject) {
-    fetch(request).then(function(response){
-      if(response.status !== 404) {
+function checkResponse(request) {
+  return new Promise(function (fulfill, reject) {
+    fetch(request).then(function (response) {
+      if (response.status !== 404) {
         fulfill(response);
       } else {
         reject();
       }
     }, reject);
   });
-};
+}
 
-const addToCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
+function addToCache(request) {
+  return caches.open("pwabuilder-offline").then(function (cache) {
     return fetch(request).then(function (response) {
-      console.log('[PWA Builder] add page to offline cache: ' + response.url);
+      console.log("[PWA Builder] add page to offline cache: " + response.url);
       return cache.put(request, response);
     });
   });
-};
+}
 
-const returnFromCache = function(request){
-  return caches.open('pwabuilder-offline').then(function (cache) {
+function returnFromCache(request) {
+  return caches.open("pwabuilder-offline").then(function (cache) {
     return cache.match(request).then(function (matching) {
-     if(!matching || matching.status == 404) {
-       return cache.match('offline.html')
-     } else {
-       return matching
-     }
+      if (!matching || matching.status == 404) {
+        return cache.match("offline.html");
+      } else {
+        return matching;
+      }
     });
   });
-};
+}
