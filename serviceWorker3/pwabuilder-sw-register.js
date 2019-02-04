076ee439208b6c9557751393e6c1847d93f251a6
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-02-04 18:14:27 -0300

    Fix registration files Format, added missing checks and remove unnecesary spaces

--- serviceWorker3/pwabuilder-sw-register.js
+++ serviceWorker3/pwabuilder-sw-register.js
@@ -1,18 +1,16 @@
-//This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
+// This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
-//Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
 
-//Check compatibility for the browser we're running this in
+// Check compatibility for the browser we're running this in
 if ('serviceWorker' in navigator) {
-
   if (navigator.serviceWorker.controller) {
     console.log('[PWA Builder] active service worker found, no need to register');
   } else {
-
-  //Register the service worker
+    // Register the service worker
     navigator.serviceWorker.register('pwabuilder-sw.js', {
       scope: './'
-    }).then(function(reg) {
+    }).then(function (reg) {
       console.log('[PWA Builder] Service worker has been registered for scope: ' + reg.scope);
     });
   }
