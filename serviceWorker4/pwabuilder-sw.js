b0fb62e537fb3868b7e060ebc401903050282bc7
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-02-04 18:23:14 -0300

    Update format in SW 4

--- serviceWorker4/pwabuilder-sw.js
+++ serviceWorker4/pwabuilder-sw.js
@@ -1,32 +1,34 @@
 //This is the service worker with the Cache-first network
 
-const CACHE = 'pwabuilder-precache';
-cosnt precacheFiles = [
-      /* Add an array of files to precache for your app */
-    ];
+const CACHE = "pwabuilder-precache";
+const precacheFiles = [
+  /* Add an array of files to precache for your app */
+];
 
 //Install stage sets up the cache-array to configure pre-cache content
-self.addEventListener('install', function(evt) {
-  console.log('[PWA Builder] The service worker is being installed.');
-  evt.waitUntil(precache().then(function() {
-    console.log('[PWA Builder] Skip waiting on install');
-    return self.skipWaiting();
-  }));
+self.addEventListener("install", function (evt) {
+  console.log("[PWA Builder] The service worker is being installed.");
+  evt.waitUntil(
+    precache().then(function () {
+      console.log("[PWA Builder] Skip waiting on install");
+      return self.skipWaiting();
+    })
+  );
 });
 
-
 //allow sw to control of current page
-self.addEventListener('activate', function(event) {
-  console.log('[PWA Builder] Claiming clients for current page');
+self.addEventListener("activate", function (event) {
+  console.log("[PWA Builder] Claiming clients for current page");
   return self.clients.claim();
 });
 
-self.addEventListener('fetch', function(evt) {
-  console.log('[PWA Builder] The service worker is serving the asset. ' + evt.request.url);
+self.addEventListener("fetch", function (evt) {
+  console.log(
+    "[PWA Builder] The service worker is serving the asset. " + evt.request.url
+  );
   evt.respondWith(fromCache(evt.request).catch(fromServer(evt.request)));
   evt.waitUntil(update(evt.request));
 });
-
 
 function precache() {
   return caches.open(CACHE).then(function (cache) {
@@ -38,13 +40,13 @@ function fromCache(request) {
   //we pull files from the cache first thing so we can show them fast
   return caches.open(CACHE).then(function (cache) {
     return cache.match(request).then(function (matching) {
-      return matching || Promise.reject('no-match');
+      return matching || Promise.reject("no-match");
     });
   });
 }
 
 function update(request) {
-  //this is where we call the server to get the newest version of the 
+  //this is where we call the server to get the newest version of the
   //file to use the next time we show view
   return caches.open(CACHE).then(function (cache) {
     return fetch(request).then(function (response) {
@@ -53,7 +55,9 @@ function update(request) {
   });
 }
 
-function fromServer(request){
+function fromServer(request) {
   //this is the fallback if it is not in the cache to go to the server and get it
-  return fetch(request).then(function(response){ return response});
+  return fetch(request).then(function (response) {
+    return response;
+  });
 }
