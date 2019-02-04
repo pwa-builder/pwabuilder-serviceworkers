9a10b842772e8ca96ff02fd3f2eac6cec56f4cf8
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-02-04 18:18:13 -0300

    Fix bad merge conflict in SW 2

--- serviceWorker2/pwabuilder-sw.js
+++ serviceWorker2/pwabuilder-sw.js
@@ -1,50 +1,55 @@
-//This is the "Offline copy of pages" service worker
+// This is the "Offline copy of pages" service worker
 
-var cacheName = 'pwabuilder-offline';
+var cacheName = "pwabuilder-offline";
 
-//Install stage sets up the index page (home page) in the cache and opens a new cache
-self.addEventListener('install', function(event) {
-  const indexPage = new Request('index.html');
-  console.log('[PWA Builder] Install');
+// Install stage sets up the index page (home page) in the cache and opens a new cache
+self.addEventListener("install", function(event) {
+  const indexPage = new Request("index.html");
+  console.log("[PWA Builder] Install");
   event.waitUntil(
     fetch(indexPage).then(function(response) {
-
       return caches.open(cacheName).then(function(cache) {
-        console.log('[PWA Builder] Cached index page during Install ' + response.url);
+        console.log(
+          "[PWA Builder] Cached index page during Install " + response.url
+        );
         return cache.put(indexPage, response);
       });
     })
   );
 });
 
-//If any fetch fails, it will look for the request in the cache and serve it from there first
-self.addEventListener('fetch', function(event) {
-  const updateCache = function(request){
-    return caches.open('pwabuilder-offline').then(function (cache) {
-  let updateCache = function(request){
-    return caches.open(cacheName).then(function (cache) {
-      return fetch(request.clone()).then(function (response) {
-        console.log('[PWA Builder] add page to offline cache: ' + response.url);
-        return cache.put(request, response);
-      });
-    });
-  };
-
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function(event) {
   event.waitUntil(updateCache(event.request));
 
   event.respondWith(
     fetch(event.request).catch(function(error) {
-      console.log('[PWA Builder] Network request Failed. Serving content from cache: ' + error);
+      console.log(
+        "[PWA Builder] Network request Failed. Serving content from cache: " +
+          error
+      );
 
-      //Check to see if you have it in the cache
-      //Return response
-      //If not in the cache, then return error page
+      // Check to see if you have it in the cache
+      // Return response
+      // If not in the cache, then return error page
       return caches.open(cacheName).then(function(cache) {
         return cache.match(event.request).then(function(matching) {
-          let report =  !matching || matching.status == 404?Promise.reject('no-match'): matching;
-          return report
+          let report =
+            !matching || matching.status == 404
+              ? Promise.reject("no-match")
+              : matching;
+          return report;
         });
       });
     })
   );
 });
+
+function updateCache(request) {
+  return caches.open(cacheName).then(function(cache) {
+    return fetch(request.clone()).then(function(response) {
+      console.log("[PWA Builder] add page to offline cache: " + response.url);
+      return cache.put(request, response);
+    });
+  });
+}
