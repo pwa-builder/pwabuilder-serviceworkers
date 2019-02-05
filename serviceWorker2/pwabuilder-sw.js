e376dee8ab3fc1a6c49b08519e59de821ce49231
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-02-05 11:34:15 -0300

    Update logic to prevent doing double fetch on each request

--- serviceWorker2/pwabuilder-sw.js
+++ serviceWorker2/pwabuilder-sw.js
@@ -1,17 +1,14 @@
 // This is the "Offline copy of pages" service worker
 
-var cacheName = "pwabuilder-offline";
+const CACHE = "pwabuilder-offline";
 
 // Install stage sets up the index page (home page) in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   const indexPage = new Request("index.html");
-  console.log("[PWA Builder] Install");
   event.waitUntil(
     fetch(indexPage).then(function (response) {
-      return caches.open(cacheName).then(function (cache) {
-        console.log(
-          "[PWA Builder] Cached index page during Install " + response.url
-        );
+      return caches.open(CACHE).then(function (cache) {
+        console.log("[PWA Builder] Cached index page during Install " + response.url);
         return cache.put(indexPage, response);
       });
     })
@@ -20,36 +17,29 @@ self.addEventListener("install", function (event) {
 
 // If any fetch fails, it will look for the request in the cache and serve it from there first
 self.addEventListener("fetch", function (event) {
-  event.waitUntil(updateCache(event.request));
-
   event.respondWith(
-    fetch(event.request).catch(function (error) {
-      console.log(
-        "[PWA Builder] Network request Failed. Serving content from cache: " +
-        error
-      );
+    caches.open(CACHE).then(function (cache) {
+      fetch(event.request)
+        .then(function (response) {
+          console.log("[PWA Builder] add page to offline cache: " + response.url);
 
-      // Check to see if you have it in the cache
-      // Return response
-      // If not in the cache, then return error page
-      return caches.open(cacheName).then(function (cache) {
-        return cache.match(event.request).then(function (matching) {
-          let report =
-            !matching || matching.status == 404
-              ? Promise.reject("no-match")
-              : matching;
-          return report;
-        });
-      });
+          // If request was success, add or update it in the cache
+          return cache.put(request, response);
+        })
+        .catch(function (error) {
+          console.log("[PWA Builder] Network request Failed. Serving content from cache: " + error);
+
+          // Check to see if you have it in the cache
+          // Return response
+          // If not in the cache, then return error page
+          return cache.match(event.request).then(function (matching) {
+            let report =
+              !matching || matching.status == 404
+                ? Promise.reject("no-match")
+                : matching;
+            return report;
+          });
+        })
     })
   );
 });
-
-function updateCache(request) {
-  return caches.open(cacheName).then(function (cache) {
-    return fetch(request.clone()).then(function (response) {
-      console.log("[PWA Builder] add page to offline cache: " + response.url);
-      return cache.put(request, response);
-    });
-  });
-}
