89b39064df4143ff3cdb429b9bc9ad49443b4d5b
Nicolas Bello Camilletti <nicolas.bello@southworks.com>
2019-02-05 15:21:33 -0300

    Update SW 1 to have the same simple approach as SW 2 and 3

--- serviceWorker1/pwabuilder-sw.js
+++ serviceWorker1/pwabuilder-sw.js
@@ -1,18 +1,16 @@
 // This is the "Offline page" service worker
 
 const CACHE = "pwabuilder-precache";
-const offlinePage = new Request("offline.html");
+const offlinePage = "index.html";
 
 // Install stage sets up the offline page in the cache and opens a new cache
 self.addEventListener("install", function (event) {
   console.log("[PWA Builder] Install Event processing");
-  
+
   event.waitUntil(
-    fetch(offlinePage).then(function (response) {
-      return caches.open(CACHE).then(function (cache) {
-        console.log("[PWA Builder] Cached offline page during install " + response.url);
-        return cache.put(offlinePage, response);
-      });
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Cached offline page during install");
+      return cache.add(offlinePage);
     })
   );
 });
@@ -31,9 +29,13 @@ self.addEventListener("fetch", function (event) {
 });
 
 // This is an event that can be fired from your page to tell the SW to update the offline page
-self.addEventListener("refreshOffline", function (response) {
-  return caches.open(CACHE).then(function (cache) {
-    console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
-    return cache.put(offlinePage, response);
+self.addEventListener("refreshOffline", function () {
+  const offlinePageRequest = new Request(offlinePage);
+
+  return fetch(offlinePage).then(function (response) {
+    return caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Offline page updated from refreshOffline event: " + response.url);
+      return cache.put(offlinePageRequest, response);
+    });
   });
 });
