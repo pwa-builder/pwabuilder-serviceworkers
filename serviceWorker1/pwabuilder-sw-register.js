31b326283ecd52f319b54aafccf94025f8e77ab9
Justin Willis <jgw9617@gmail.com>
2020-03-07 14:59:56 -0800

    feat(): use pwa-update for registration

--- serviceWorker1/pwabuilder-sw-register.js
+++ serviceWorker1/pwabuilder-sw-register.js
@@ -1,19 +1,8 @@
 // This is the "Offline page" service worker
 
-// Add this below content to your HTML page, or add the js file to your page at the very top to register service worker
+// Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
 
-// Check compatibility for the browser we're running this in
-if ("serviceWorker" in navigator) {
-  if (navigator.serviceWorker.controller) {
-    console.log("[PWA Builder] active service worker found, no need to register");
-  } else {
-    // Register the service worker
-    navigator.serviceWorker
-      .register("pwabuilder-sw.js", {
-        scope: "./"
-      })
-      .then(function (reg) {
-        console.log("[PWA Builder] Service worker has been registered for scope: " + reg.scope);
-      });
-  }
-}
+import 'https://cdn.jsdelivr.net/npm/@pwabuilder/pwaupdate';
+
+const el = document.createElement('pwa-update');
+document.body.appendChild(el);
