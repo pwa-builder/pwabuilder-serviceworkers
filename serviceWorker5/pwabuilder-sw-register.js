b1cb54012ab0d7f45e0979adad64165287d754a9
Justin Willis <jgw9617@gmail.com>
2020-03-12 10:55:21 -0700

    chore(): update with info about type=module

--- serviceWorker5/pwabuilder-sw-register.js
+++ serviceWorker5/pwabuilder-sw-register.js
@@ -1,6 +1,7 @@
 // This is the service worker with the Advanced caching
 
 // Add this below content to your HTML page inside a <script type="module"></script> tag, or add the js file to your page at the very top to register service worker
+// If you get an error about not being able to import, double check that you have type="module" on your <script /> tag
 
 /*
  This code uses the pwa-update web component https://github.com/pwa-builder/pwa-update to register your service worker,
