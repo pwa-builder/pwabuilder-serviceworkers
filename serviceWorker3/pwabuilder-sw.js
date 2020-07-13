11cd240c03f1b8985fd4becce7e3eddf6895d21e
enolife <enouwemjim2@gmail.com>
2020-07-13 11:47:29 +0100

    fix: add missing workbox script

--- serviceWorker3/pwabuilder-sw.js
+++ serviceWorker3/pwabuilder-sw.js
@@ -1,5 +1,7 @@
 // This is the service worker with the combined offline experience (Offline page + Offline copy of pages)
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
 const CACHE = "pwabuilder-offline-page";
 
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
