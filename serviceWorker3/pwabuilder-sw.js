60c20d680e761c128f2f3453a0ce61ca2b764dd7
jgw9617 <jgw9617@gmail.com>
2020-09-21 12:33:26 -0700

    chore(): add import statement

--- serviceWorker3/pwabuilder-sw.js
+++ serviceWorker3/pwabuilder-sw.js
@@ -2,6 +2,8 @@
 
 const CACHE = "pwabuilder-offline-page";
 
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.0.0/workbox-sw.js');
+
 // TODO: replace the following with the correct offline fallback page i.e.: const offlineFallbackPage = "offline.html";
 const offlineFallbackPage = "ToDo-replace-this-name.html";
 
