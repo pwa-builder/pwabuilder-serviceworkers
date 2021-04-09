b761bb2bc7ef20f2a890bdaea7a3c1343a5cd734
Arpit <arpit@reddingdesigns.com>
2021-04-09 10:51:19 -0300

    fix https://github.com/pwa-builder/PWABuilder/issues/1618

--- serviceWorker6/pwabuilder-sw.js
+++ serviceWorker6/pwabuilder-sw.js
@@ -11,7 +11,7 @@ self.addEventListener("message", (event) => {
   }
 });
 
-const bgSyncPlugin = new workbox.backgroundSync.Plugin(QUEUE_NAME, {
+const bgSyncPlugin = new workbox.backgroundSync.BackgroundSyncPlugin(QUEUE_NAME, {
   maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
 });
 
